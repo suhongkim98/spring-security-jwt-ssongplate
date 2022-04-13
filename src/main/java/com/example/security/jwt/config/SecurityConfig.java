c09df7e61c7a56722a47f534ebf9491ea9ae0266
suhongkim98 <kshz9815@naver.com>
2022-04-13 19:50:38 +0900

    인증 인가 핸들러 구현 및 spring security config 설정

--- src/main/java/com/example/security/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/security/jwt/config/SecurityConfig.java
@@ -1,32 +1,92 @@
 package com.example.security.jwt.config;
 
+import com.example.security.jwt.security.handler.JwtAccessDeniedHandler;
+import com.example.security.jwt.security.handler.JwtAuthenticationEntryPoint;
+import com.example.security.jwt.security.JwtSecurityConfig;
+import com.example.security.jwt.security.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 // 추가적인 시큐리티 설정을 위해 WebSecurityConfigurer를 implements 하거나 WebSecurityConfigurerAdapter를 extends하는 방법이 있는데
 // 여기서는 WebSecurityConfigurerAdapter를 extends 하는 방법 사용
-@EnableWebSecurity // 기본적인 웹보안을 활성화하겠다.
+@EnableWebSecurity // 기본적인 웹보안을 활성화하겠다
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션 사용을 위해 선언
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    // 생성자 통해 스프링 빈 주입받는다.
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    // BCryptPasswordEncoder 라는 패스워드 인코더 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) {
-        // embedded h2 console 접근에 원활히 하기 위해 h2 콘솔 하위 요청과, favicon은 시큐리티 적용하지않음
-        //
-        web
-                .ignoring()
+        web.ignoring()
                 .antMatchers(
-                        "/h2/**",
-                        "/favicon.ico"
+                        "/h2/**"
+                        ,"/favicon.ico"
+                        ,"/error"
                 );
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests() // HttpServletRequest를 사용하는 요청에 대한 접근 제한을 설정하겠다.
-                .antMatchers("/api/hello").permitAll() // /api/hello 경로에 대해서는 인증없이 접근 허용
-                .anyRequest().authenticated(); // 나머지 요청에 대해서는 인증을 받아야한다.
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint) // 우리가 만든 클래스로 인증 실패 핸들링
+                .accessDeniedHandler(jwtAccessDeniedHandler) // 커스텀 인가 실패 핸들링
+
+                // enable h2-console // embedded h2를 위한 설정
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll() // /api/hello
+                .antMatchers("/api/authenticate").permitAll() // 로그인 경로
+                .antMatchers("/api/signup").permitAll() // 회원가입 경로는 인증없이 호출 가능
+
+                .anyRequest().authenticated() // 나머지 경로는 jwt 인증 해야함
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider)); // JwtSecurityConfig 적용
     }
 }
---
+++ src/main/java/com/example/security/jwt/security/JwtFilter.java
@@ -0,0 +1,58 @@
+package com.example.security.jwt.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    // 실제 필터링 로직은 doFilter 안에 들어가게 된다. GenericFilterBean을 받아 구현
+    // Dofilter는 토큰의 인증정보를 SecurityContext 안에 저장하는 역할 수행
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String jwt = resolveToken(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
+
+        // 유효성 검증
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+        } else {
+            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    // 헤더에서 토큰 정보를 꺼내온다.
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
