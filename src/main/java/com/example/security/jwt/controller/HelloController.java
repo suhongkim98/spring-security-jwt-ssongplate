094fb479d04a1ab8aac0a5a57504496109804859
suhongkim98 <kshz9815@naver.com>
2022-04-13 19:57:43 +0900

    JWT 인가 테스트

--- src/main/java/com/example/security/jwt/controller/HelloController.java
+++ src/main/java/com/example/security/jwt/controller/HelloController.java
@@ -1,16 +1,46 @@
 package com.example.security.jwt.controller;
 
+import com.example.security.jwt.controller.dto.ResponseUser;
+import com.example.security.jwt.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+    private final UserService userService;
+
+    public HelloController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("hello");
+    }
+
+    // redirect test
+    @PostMapping("/test-redirect")
+    public void testRedirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/api/user");
+    }
+
+    // 인가 테스트
+    // authorization: Bearer {AccessToken}
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')") // USER, ADMIN 권한 둘 다 호출 허용
+    public ResponseEntity<ResponseUser.Info> getMyUserInfo(HttpServletRequest request) {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // ADMIN 권한만 호출 가능
+    public ResponseEntity<ResponseUser.Info> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username));
     }
 }
