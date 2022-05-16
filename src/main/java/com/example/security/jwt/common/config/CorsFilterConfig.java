7fce5974eb74c97d1a1ad2f0fb9a2aa928182387
suhongkim98 <kshz9815@naver.com>
2022-05-16 22:17:05 +0900

    cors origin pattern 설정

--- src/main/java/com/example/security/jwt/common/config/CorsFilterConfig.java
+++ src/main/java/com/example/security/jwt/common/config/CorsFilterConfig.java
@@ -14,7 +14,7 @@ public class CorsFilterConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
